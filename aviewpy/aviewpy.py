82684222d953c4c21f140ce9cac7870111e6ab97
bthornton191 <ben.thornton@mscsoftware.com>
2019-01-30 14:58:26 -0600

    Intial commit

---
+++ aviewpy/aviewpy.py
@@ -0,0 +1,7 @@
+"""Python tools for working in the adams view python environment.
+
+Authors:
+----------
+Vishnu Balakrishnan - Consultant, MSC Software
+Ben Thornton - Consultant, MSC Software
+"""
