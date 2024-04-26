92e8f65704c569715ff5e142f64151119a3d2115
Ben Thornton <ben.thornton@hexagon.com>
2024-04-26 16:27:56 -0400

    Update version to 1.3.6 and added versions notes

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,7 +1,16 @@
 # ---------------------
-version = '1.3.5'
-version_notes = 'improvements to `set_dv`, `ui.messages.show_message` now respects message threshold'
-date = 'September 1st, 2023'
+version = '1.3.6'
+version_notes = (
+    'Add ignore_parse parameter to get_sim_errors function\n'
+    'Add logging and modify submit function\n'
+    'Add turn_on_all_force_graphics module\n'
+    'Add show_fbd function to display Free Body Diagram\n'
+    'Formatting and changed "aview_main" to "__main__"\n'
+    'Fix comparison operators in res_to_csv.py\n'
+    'Add show_fbd function to create animation in Adams post processor\n'
+    'Replaced Object class with ObjectComment class in variables.py. Added an adams_warning_suppressed context manager to alerts.py\n'
+)
+date = 'April 26th, 2024'
 # ---------------------
 author = 'Ben Thornton'
 author_email = 'ben.thornton@hexagon.com'
